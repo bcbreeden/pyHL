0194f11dd47926c1940721ebf1a87355ec751ecc
Brian Breeden <bcbreeden@proton.me>
2023-05-06 14:27:08 -0400

    added schedule error handling

--- data/api/schedule.py
+++ data/api/schedule.py
@@ -1,5 +1,24 @@
 import json
 import requests
 
+'''
+Returns the schedule as a Pandas Dataframe.
+
+Requires a season in the StartYearEndYear (20182019) format.
+
+Acceptable game types:
+    * PR - Preseason
+    * R - Regular Season
+    * P - Playoffs
+'''
 def get_schedule(season, game_type):
+    if game_type.upper() not in ['PR', 'R', 'P']:
+        print('Invalid gametype')
+        return 0
+    
+    if len(str(season)) != 8:
+        print("Invalid season")
+        return 0
+
+    api_string = "https://statsapi.web.nhl.com/api/v1/schedule?season={}&gameType={}".format(str(season), game_type)
     return False
--- data/test_schedule.py
+++ data/test_schedule.py
@@ -4,3 +4,10 @@ def test_get_schedule_length():
     schedule_df = get_schedule()
     assert len(schedule_df.index) > 0
 
+def test_invalid_schedule_game_type():
+    error_code = get_schedule(20192020, "PreSeason")
+    assert error_code == 0
+
+def test_invalid_schedule_season():
+    error_code = get_schedule(2020, "pr")
+    assert error_code == 0
